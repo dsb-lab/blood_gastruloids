39206189c02d27df28122eea9260745371eca57f
Gabriel Torregrosa <g.torregrosa@outlook.com>
2022-08-23 16:35:43 +0200

    Setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ cd Data_other_studies/Fadlullah
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE150nnn/GSE150412/suppl/GSE150412_RawCounts.csv.gz
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE150nnn/GSE150412/matrix/GSE150412-GPL19057_series_matrix.txt.gz
 gunzip *
+mkdir Plots
+mkdir Tables
 cd ../..
 
 #Download Vink Dataset
@@ -12,6 +14,8 @@ cd Data_other_studies/Vink
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE143nnn/GSE143637/suppl/GSE143637_HTSeq_counts.txt.gz
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE143nnn/GSE143637/matrix/GSE143637_series_matrix.txt.gz
 gunzip *
+mkdir Plots
+mkdir Tables
 cd ../..
 
 #Download data Analysis 2021
